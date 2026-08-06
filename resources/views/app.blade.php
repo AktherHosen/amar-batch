f20d93a28b7a34c51bcce2f58880dba93aa59379
aktherhosen <dev.aktherhosen@gmail.com>
2026-08-06 20:10:10 +0600

    Update manifest and welcome page styles; add new icons for PWA support
    - Updated the manifest.json to replace existing icons with new SVG and PNG files.
    - Added new web app manifest images: web-app-manifest-192x192.png and web-app-manifest-512x512.png.
    - Enhanced the welcome page styles, changing header and button colors to a blue theme.
    - Adjusted section backgrounds and text colors to align with the new design.
    - Added favicon-96x96.png to the app.blade.php for improved icon support.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,6 +32,7 @@
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon-96x96.png" type="image/png" sizes="96x96">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         {{-- PWA --}}
