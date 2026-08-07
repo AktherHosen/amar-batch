aab346107b045e08e8c07af3dee42210bc8f61b3
aktherhosen <dev.aktherhosen@gmail.com>
2026-08-07 23:02:02 +0600

    feat: add action_date column to batch_histories table; update CoachingClassSeeder and StudentSeeder with fee adjustments and student data corrections

--- database/seeders/CoachingClassSeeder.php
+++ database/seeders/CoachingClassSeeder.php
@@ -10,13 +10,14 @@ class CoachingClassSeeder extends Seeder
     public function run(): void
     {
         $classes = [
+            ['name' => 'Pre School', 'default_fee' => 300],
             ['name' => 'Nursery', 'default_fee' => 300],
             ['name' => 'KG', 'default_fee' => 300],
-            ['name' => 'Class 1', 'default_fee' => 500],
+            ['name' => 'Class 1', 'default_fee' => 400],
             ['name' => 'Class 2', 'default_fee' => 500],
             ['name' => 'Class 3', 'default_fee' => 500],
             ['name' => 'Class 4', 'default_fee' => 500],
-            ['name' => 'Class 5', 'default_fee' => 500],
+            ['name' => 'Class 5', 'default_fee' => 700],
         ];
 
         foreach ($classes as $class) {
