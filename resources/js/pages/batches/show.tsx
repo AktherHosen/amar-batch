236f2eb829e91c0078d67624d899b1f365175819
aktherhosen <dev.aktherhosen@gmail.com>
2026-09-16 21:13:46 +0600

    feat: add batch detail page with student enrollment and teacher management features

--- resources/js/pages/batches/show.tsx
+++ resources/js/pages/batches/show.tsx
@@ -1346,7 +1346,7 @@ export default function BatchesShow({
                         </DialogDescription>
                     </DialogHeader>
                     <div className="space-y-4 py-2">
-                        <div className="space-y-2">
+                        <div className="flex flex-col gap-2">
                             <label className="text-sm font-medium">
                                 {t('batches.select_target_batch') ?? 'Target Batch'}
                             </label>
